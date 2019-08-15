4bad3710dd5f04e4db690ed744db5e8e122d3bf3
xiaiyu <1141600195@qq.com>
2019-08-15 14:32:25 +0800

    添加权限增删改

--- fushuai-dmanager/src/main/java/com/manager/web/MenuController.java
+++ fushuai-dmanager/src/main/java/com/manager/web/MenuController.java
@@ -13,12 +13,17 @@ package com.manager.web;
 
 import com.kh.pojo.ResponseResult;
 import com.kh.pojo.entity.MenuInfo;
+import com.kh.utils.TwitterIdWorker;
+import com.manager.dao.MenuDao;
 import com.manager.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -35,6 +40,9 @@ public class MenuController {
     @Autowired
     private MenuService menuService;
 
+    @Autowired
+    private MenuDao menuDao;
+
     //获取权限列表
     @RequestMapping("menuList")
     @ResponseBody
@@ -47,5 +55,41 @@ public class MenuController {
         responseResult.setSuccess("列表返回成功");
         return responseResult;
     }
+
+
+    @RequestMapping("/addMenu")
+    @ResponseBody
+    public ResponseResult addMenu(@RequestBody MenuInfo menuInfo){
+        ResponseResult responseResult=ResponseResult.getResponseResult();
+        TwitterIdWorker twitterIdWorker=new TwitterIdWorker();
+        long id = twitterIdWorker.nextId();
+        menuInfo.setId(id);
+        menuDao.save(menuInfo);
+        menuDao.flush();
+        responseResult.setCode(200);
+        return responseResult;
+    }
+    @RequestMapping("/updateMenu")
+    @ResponseBody
+    public ResponseResult updateMenu(@RequestBody MenuInfo menuInfo){
+        ResponseResult responseResult=ResponseResult.getResponseResult();
+        menuDao.save(menuInfo);
+        menuDao.flush();
+        responseResult.setCode(200);
+        return responseResult;
+    }
+    @Transactional
+    @RequestMapping("/delMenu")
+    @ResponseBody
+    public ResponseResult delMenu(@RequestBody MenuInfo menuInfo){
+        ResponseResult responseResult=ResponseResult.getResponseResult();
+        Long[] ids = menuInfo.getIds();
+        List<MenuInfo> allById = menuDao.findAllById(Arrays.asList(ids));
+        menuDao.deleteAll(allById);
+        responseResult.setCode(200);
+        return responseResult;
+    }
+
+
 
 }
