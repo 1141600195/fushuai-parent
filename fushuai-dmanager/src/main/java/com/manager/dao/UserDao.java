3b216cd99264eaa4722c49c0da5a4b1654cb5809
xiaiyu <1141600195@qq.com>
2019-08-20 14:16:03 +0800

    增加通过id查找用户

--- fushuai-dmanager/src/main/java/com/manager/dao/UserDao.java
+++ fushuai-dmanager/src/main/java/com/manager/dao/UserDao.java
@@ -9,12 +9,17 @@ import java.util.List;
 /**
  *
  */
-public interface UserDao extends JpaRepository<UserInfo,Long> {
-    @Query(value ="select * from base_user where loginName=?1",nativeQuery =true)
+public interface UserDao extends JpaRepository<UserInfo, Long> {
+    @Query(value = "select * from base_user where loginName=?1", nativeQuery = true)
     public UserInfo findByLoginName(String loginName);
 
 
-    @Query(value = "select bu.* from base_user_role bur inner  join base_user bu on bur.userId=bu.id where bur.roleId=?1",nativeQuery = true)
+    @Query(value = "select bu.* from base_user_role bur inner  join base_user bu on bur.userId=bu.id where bur.roleId=?1", nativeQuery = true)
     public List<UserInfo> forUserInfoByUserId(Long roleId);
+
+    public UserInfo findByEmail(String email);
+
+    @Query(value = "select * from base_user where id=?1", nativeQuery = true)
+    public UserInfo selectByUserinfoById(Long id);
 
 }
